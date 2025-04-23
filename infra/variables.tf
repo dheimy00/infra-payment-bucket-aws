14d20760f55caf65d25f8b1c1ca1e8dfeaa69c30
Dheimy <jheimyeng@hotmail.com>
2025-04-23 19:13:06 -0300

    Add env

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,3 +1,31 @@
 variable "bucket_name" {
-  description = "The name bucket"
+  type = string
+}
+
+variable "versioning_enabled" {
+  type    = bool
+  default = false
+}
+
+variable "encryption_algorithm" {
+  type    = string
+  default = "AES256"
+}
+
+variable "tags" {
+  type    = map(string)
+  default = {}
+}
+
+variable "lifecycle_rules" {
+  type = list(object({
+    id              = string
+    status          = string
+    expiration_days = optional(number)
+    transitions     = optional(list(object({
+      days          = number
+      storage_class = string
+    })))
+  }))
+  default = []
 }
