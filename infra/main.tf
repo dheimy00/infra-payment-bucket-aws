83fbdd2d7e2ac313874c94b1f74883ca5699546d
Dheimy <jheimyeng@hotmail.com>
2025-04-23 18:21:49 -0300

    Updated version

--- infra/main.tf
+++ infra/main.tf
@@ -1,5 +1,5 @@
 module "s3_bucket" {
-  source = "git::https://github.com/dheimy00/modules-infra-bucket-S3-aws.git?ref=v1.0.1"
+  source = "git::https://${GH_TOKEN}@github.com/dheimy00/modules-infra-bucket-S3-aws.git?ref=v1.0.1"
 
   bucket_name = "payment-bucket-${random_string.suffix.result}"
   versioning_enabled = true
