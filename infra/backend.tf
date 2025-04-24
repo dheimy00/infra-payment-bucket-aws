1a4183df316fc1d33f892d9b8bdc553651c29d17
Dheimy <jheimyeng@hotmail.com>
2025-04-24 02:17:27 -0300

    Destroy

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,3 +1,9 @@
 terraform {
-  backend "s3" {}
+  backend "s3" {
+    bucket         = "dheimy00-us-east-2-terraform-statefile"
+    key            = "payment-bucket/terraform.tfstate"
+    region         = "us-east-2"
+    dynamodb_table = "dheimy00-us-east-2-terraform-lock"
+    encrypt        = true
+  }
 }
