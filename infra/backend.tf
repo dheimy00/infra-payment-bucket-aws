7403d79c4d0d206020b9070d945ea1110efa5f5c
Dheimy <jheimyeng@hotmail.com>
2025-04-24 00:22:00 -0300

    Apply

--- infra/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    # These values will be filled in by the user when initializing the backend
-    bucket         = "dheimy00-us-east-2-terraform-statefile"
-    region         = "us-east-2"
-    dynamodb_table = "dheimy00-us-east-2-terraform-lock"
-  }
-} 
